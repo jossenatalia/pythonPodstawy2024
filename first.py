22df2fbb4942e7cd37d0b724423f20cb2614edd2
Natalia Josse <natalia.josse@comarch.pl>
2024-02-26 11:01:54 +0100

    First commit with data

---
+++ first.py
@@ -0,0 +1,6 @@
+print()  # wydrukuj/wypisz
+print("First printed value")
+print("Second printed value")
+print('Third printed value')
+# type() wypisanie typu danej
+print(type('Third printed value'))
