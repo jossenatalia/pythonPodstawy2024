f3a17ac3514c8d44835151a203f5bf67e97f68c9
Natalia Josse <natalia.josse@comarch.pl>
2024-02-27 10:46:50 +0100

    wyjątki

--- wyjątki.py
+++ wyjątki.py
@@ -26,23 +26,30 @@ print('''
 4. Dzielenie
 ''')
 
-a = int(input("Podaj liczbę a"))
-b = int(input("Podaj liczbę b"))
-operacja = int(input(f"Wybierz działanie matematyczne"))
+try:
+    a = int(input("Podaj liczbę a"))
+    b = int(input("Podaj liczbę b"))
+    operacja = int(input(f"Wybierz działanie matematyczne"))
 
-
-match operacja:
-    case 1:
-        print(f"Wynik działania: {a + b}")
-    case 2:
-        print(f"Wynik działania: {a - b}")
-    case 3:
-        print(f"Wynik działania: {a * b}")
-    case 4:
-        try:
-            print(f"Wynik działania: {a / b}")
-        except ZeroDivisionError:
-            print("Nie dziel przez 0")
-    case _:
-        print("Nie znam takiego działania")
-
+    match operacja:
+        case 1:
+            print(f"Wynik działania: {a + b}")
+        case 2:
+            print(f"Wynik działania: {a - b}")
+        case 3:
+            print(f"Wynik działania: {a * b}")
+        case 4:
+            try:
+                print(f"Wynik działania: {a / b}")
+            except ZeroDivisionError:
+                print("Nie dziel przez 0")
+        case _:
+            print("Nie znam takiego działania")
+except ValueError as e:
+    print("Błąd wartości", e)
+except TypeError as e:
+    print("Błąd typu", e)
+except ZeroDivisionError as e:
+    print("Nie dziel przez zero", e)
+except Exception as e:
+    print("Błąd", e)
