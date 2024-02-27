9e238f57d59ad98dc49b01a8ce5eb8f3e18c7752
Natalia Josse <natalia.josse@comarch.pl>
2024-02-27 14:05:55 +0100

    encoding

---
+++ files2.py
@@ -0,0 +1,13 @@
+import chardet
+
+file_path = 'test.log'
+with open(file_path, "rb") as file:
+    raw_data = file.read()
+
+result = chardet.detect(raw_data)
+print(result)
+
+encoding = result['encoding']
+print(raw_data.decode(encoding=encoding))
+
+
