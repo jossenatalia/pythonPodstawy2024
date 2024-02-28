7b4db68c31f6b83648482910798138e435147e9b
Natalia Josse <natalia.josse@comarch.pl>
2024-02-28 12:41:41 +0100

    generatory next

--- generatory/generatory.py
+++ generatory/generatory.py
@@ -15,14 +15,14 @@ def kwadrat2(n):
 
 
 
-kwadrat(5)
-kwa = kwadrat2(5)
-print(type(kwa))
-print(next(kwa))
-print(next(kwa))
-print(next(kwa))
-print(next(kwa))
-print(next(kwa))
+# kwadrat(5)
+# kwa = kwadrat2(5)
+# print(type(kwa))
+# print(next(kwa))
+# print(next(kwa))
+# print(next(kwa))
+# print(next(kwa))
+# print(next(kwa))
 # print(next(kwa))  #StopIteration gdy koniec iteracji
 
 
@@ -40,32 +40,44 @@ def counter_down(min):
         count -= 1
 
 
-c = counter()
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
-print(next(c))
+# c = counter()
+# print(next(c))
+# print(next(c))
+# print(next(c))
+# print(next(c))
+# print(next(c))
+# print(next(c))
+#
+# cd = counter_down(10)
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
 
-cd = counter_down(10)
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
-print(next(cd))
+def counter_2(start=0):
+    n = start
+    while True:
+        result = yield n
+        print(result)
+        if result == 'q':
+            break;
+        n += 1
+
+
+c3 = counter_2(10)
+print(next(c3))
+print(next(c3))  # None
+print(next(c3))
+print(next(c3))
+c3.send('OK')
+print(next(c3))
+c3.send('q')
+# print(next(c3)) StopIteration
 
 
 
